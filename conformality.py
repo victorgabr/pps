Fixed: Conformity index calculation on complex structures, structure volume calculation using a correct area calculation formula, added end capping and up sampling on small structures. Example scoring validated against PlanIQ results.

conformality.py
@@ -8,7 +8,60 @@ from matplotlib.path import Path
 import numpy.ma as ma
 import numpy as np
 
+
 # TODO REFACTOR CALCULATE CI to calculate up sampled structures
+# def calc_conformation_index(rtdose, structure, lowerlimit):
+#     """From a selected structure and isodose line, return conformality index."""
+#     # Read "A simple scoring ratio to index the conformity of radiosurgical
+#     # treatment plans" by Ian Paddick.
+#     # J Neurosurg (Suppl 3) 93:219-222, 2000
+#
+#     sPlanes = structure['planes']
+#
+#     # Get the dose to pixel LUT
+#     doselut = rtdose.GetPatientToPixelLUT()
+#
+#     # Generate a 2d mesh grid to create a polygon mask in dose coordinates
+#     # Code taken from Stack Overflow Answer from Joe Kington:
+#     # http://stackoverflow.com/questions/3654289/scipy-create-2d-polygon-mask/3655582
+#     # Create vertex coordinates for each grid cell
+#     x, y = np.meshgrid(np.array(doselut[0]), np.array(doselut[1]))
+#     x, y = x.flatten(), y.flatten()
+#     dosegridpoints = np.vstack((x, y)).T
+#
+#     # Get the dose and image data information
+#     dd = rtdose.GetDoseData()
+#     id = rtdose.GetImageData()
+#
+#     PITV = 0  # Rx isodose volume in cc
+#     CV = 0  # coverage volume
+#
+#     # Iterate over each plane in the structure
+#     for z, sPlane in sPlanes.items():
+#
+#         # Get the contours with calculated areas and the largest contour index
+#         contours, largestIndex = calculate_contour_areas(sPlane)
+#
+#         # Get the dose plane for the current structure plane
+#         doseplane = rtdose.GetDoseGrid(z) * dd['dosegridscaling'] * 100
+#
+#         # If there is no dose for the current plane, go to the next plane
+#         if not len(doseplane):
+#             break
+#
+#         # Calculate the histogram for each contour
+#         for i, contour in enumerate(contours):
+#             m = get_contour_mask(doselut, dosegridpoints, contour['data'])
+#             PITV_vol, CV_vol = calculate_volume(m, doseplane, lowerlimit,
+#                                                 dd, id, structure)
+#             PITV = PITV + PITV_vol
+#             CV = CV + CV_vol
+#
+#     # Volume units are given in cm^3
+#     PITV /= 1000.0
+#     CV /= 1000.0
+#
+#     return PITV, CV
 def CalculateCI(rtdose, structure, lowerlimit):
     """From a selected structure and isodose line, return conformality index."""
     # Read "A simple scoring ratio to index the conformity of radiosurgical
@@ -117,7 +170,7 @@ def calculate_volume(mask, doseplane, lowerlimit, dd, id, structure):
     PITV_vol = np.sum(doseplane > lowerlimit) * ((id['pixelspacing'][0]) *
                                                  (id['pixelspacing'][1]) *
                                                  (structure['thickness']))
-    CV_vol = np.sum(mask > lowerlimit) * ((id['pixelspacing'][0]) *
+    CV_vol = np.sum(mask.compressed() > lowerlimit) * ((id['pixelspacing'][0]) *
                                           (id['pixelspacing'][1]) *
                                           (structure['thickness']))
     return PITV_vol, CV_vol

dev/geometry.py
@@ -26,7 +26,9 @@ from math import factorial
 
 import numba as nb
 import numpy as np
-from shapely.geometry import Polygon
+
+
+# from shapely.geometry import Polygon
 
 
 def cn_PnPoly(P, V):
@@ -378,39 +380,41 @@ def k_nearest_neighbors(k, feature_train, features_query):
     return neighbors
 
 
-def expand_roi(roi_contours, delta):
-    """
-        Expand ROI contours by delta in X, Y and Z axis.
-
-
-    :param roi_contours: list of each plane's x,y,z points
-    :param delta: delta isotropic expansion
-    :return: expanded ROI
-    """
-
-    start_cap = roi_contours[0].copy()
-    start_cap[:, 2] = start_cap[:, 2] - delta
-
-    end_cap = roi_contours[-1].copy()
-    end_cap[:, 2] = end_cap[:, 2] + delta
-
-    # extending end caps to original plans
-
-    # roi_contours[0] = start_cap
-    # roi_contours[-1] = end_cap
-    contour_tmp = [start_cap] + roi_contours + [end_cap]
-
-    # contour_tmp = roi_contours
-    res = []
-    for plane in contour_tmp:
-        ctr = Polygon(plane)
-        dilated = ctr.buffer(delta)
-        dilated_xy = np.array(dilated.exterior.coords)
-        zaxis = np.ones((len(dilated_xy), 1)) * plane[0, 2]
-        tmp = np.concatenate([dilated_xy, zaxis], axis=1)
-        res.append(tmp)
-
-    return res
+#
+#
+# def expand_roi(roi_contours, delta):
+#     """
+#         Expand ROI contours by delta in X, Y and Z axis.
+#
+#
+#     :param roi_contours: list of each plane's x,y,z points
+#     :param delta: delta isotropic expansion
+#     :return: expanded ROI
+#     """
+#
+#     start_cap = roi_contours[0].copy()
+#     start_cap[:, 2] = start_cap[:, 2] - delta
+#
+#     end_cap = roi_contours[-1].copy()
+#     end_cap[:, 2] = end_cap[:, 2] + delta
+#
+#     # extending end caps to original plans
+#
+#     # roi_contours[0] = start_cap
+#     # roi_contours[-1] = end_cap
+#     contour_tmp = [start_cap] + roi_contours + [end_cap]
+#
+#     # contour_tmp = roi_contours
+#     res = []
+#     for plane in contour_tmp:
+#         ctr = Polygon(plane)
+#         dilated = ctr.buffer(delta)
+#         dilated_xy = np.array(dilated.exterior.coords)
+#         zaxis = np.ones((len(dilated_xy), 1)) * plane[0, 2]
+#         tmp = np.concatenate([dilated_xy, zaxis], axis=1)
+#         res.append(tmp)
+#
+#     return res
 
 
 def calculate_planes_contour_areas(planes):
@@ -652,14 +656,14 @@ def get_dose_grid_3d(grid_3d, delta_mm=(2, 2, 2)):
 
 
 @nb.njit(nb.double(nb.double[:], nb.double[:]))
-def calc_area(xi, yi):
+def calc_area(x, y):
     cArea = 0
-    # xi = np.zeros(len(x) + 1)
-    # yi = np.zeros(len(y) + 1)
-    # xi[:-1] = x
-    # xi[-1] = x[0]
-    # yi[:-1] = y
-    # yi[-1] = y[0]
+    xi = np.zeros(len(x) + 1)
+    yi = np.zeros(len(y) + 1)
+    xi[:-1] = x
+    xi[-1] = x[0]
+    yi[:-1] = y
+    yi[-1] = y[0]
 
     # Calculate the area based on the Surveyor's formula
     for i in range(0, len(xi) - 1):
@@ -906,17 +910,8 @@ def calculate_structure_volume(structure):
         largestIndex = 0
         for c, contour in enumerate(sPlane):
             # Create arrays for the x,y coordinate pair for the triangulation
-            x = []
-            y = []
-            for point in contour['contourData']:
-                x.append(point[0])
-                y.append(point[1])
-
-            # cArea = 0
-
-            # for i in range(0, len(x) - 1):
-            #     cArea = cArea + x[i] * y[i + 1] - x[i + 1] * y[i]
-            # cArea = abs(cArea / 2)
+            x = contour['contourData'][:, 0]
+            y = contour['contourData'][:, 1]
             # # Calculate the area based on the Surveyor's formula
             cArea = calc_area(np.asarray(x), np.asarray(y))
 
@@ -1041,3 +1036,32 @@ def get_z_planes_dict(struc_planes, ordered_z, z_interp_positions):
             result += [struc_planes[neighbor]]
 
     return result
+
+
+def calculate_contour_areas_numba(plane):
+    """Calculate the area of each contour for the given plane.
+       Additionally calculate and return the largest contour index."""
+
+    # Calculate the area for each contour in the current plane
+    contours = []
+    largest = 0
+    largestIndex = 0
+    for c, contour in enumerate(plane):
+        # Create arrays for the x,y coordinate pair for the triangulation
+        x = contour['contourData'][:, 0]
+        y = contour['contourData'][:, 1]
+
+        cArea = calc_area(x, y)
+        # cArea = poly_area(x, y)
+        # Remove the z coordinate from the xyz point tuple
+        data = np.asarray(list(map(lambda x: x[0:2], contour['contourData'])))
+
+        # Add the contour area and points to the list of contours
+        contours.append({'area': cArea, 'data': data})
+
+        # Determine which contour is the largest
+        if cArea > largest:
+            largest = cArea
+            largestIndex = c
+
+    return contours, largestIndex

dev/validation.py
@@ -21,7 +21,6 @@ from scoring import DVHMetrics
 def test_real_dvh():
     # rs_file = r'/home/victor/Dropbox/Plan_Competition_Project/FantomaPQRT/RS.PQRT END TO END.dcm'
     # rd_file = r'/home/victor/Dropbox/Plan_Competition_Project/FantomaPQRT/RD.PQRT END TO END.Dose_PLAN.dcm'
-    up = (0.5, 0.5, 0.5)
     # rd_file = r'/home/victor/Dropbox/Plan_Competition_Project/Eclipse Plans/Venessa IMRT Eclipse/RD-Eclipse-Venessa-IMRTDose.dcm'
     # rs_file = r'/home/victor/Dropbox/Plan_Competition_Project/Competition Package/DICOM Sets/RS.1.2.246.352.71.4.584747638204.208628.20160204185543.dcm'
 
@@ -44,7 +43,7 @@ def test_real_dvh():
                 struc_teste = Structure(structure)
 
                 ecl_dvh = ecl_DVH[structure['id']]['data']
-                dhist, chist = struc_teste.calculate_dvh(dose, upsample=True, delta_cm=up)
+                dhist, chist = struc_teste.calculate_dvh(dose, upsample=True)
                 plt.figure()
                 plt.plot(chist, label='Up Sampled Structure')
                 plt.hold(True)
@@ -178,7 +177,6 @@ def test1():
     constrains['Dcc'] = 0.03
 
     st = 2
-    up = (0.4, 0.4, 0.1)
     df_concat = []
     sname = []
     # GET CALCULATED DATA
@@ -199,7 +197,8 @@ def test1():
 
         # set up sampled structure
         struc_teste = Structure(structure, end_cap=True)
-        dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True, delta_cm=up)
+        struc_teste.set_delta((0.2, 0.2, 0.1))
+        dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True)
         dvh_data = prepare_dvh_data(dhist, chist)
 
         # Setup DVH metrics class and get DVH DATA
@@ -239,7 +238,6 @@ def test1():
         res[col] = {'count': count, 'range': rg}
 
     test_table = pd.DataFrame(res).T
-    print(test_table)
 
     return test_table
 
@@ -260,7 +258,6 @@ def test2():
     # dose_grid_dir = r'D:\Dropbox\Plan_Competition_Project\testdata\DVH-Analysis-Data-Etc\DOSE GRIDS'
 
     st = 2
-    up = (0.1, 0.1, 0.1)
 
     snames = ['Sphere_10_0', 'Sphere_20_0', 'Sphere_30_0',
               'Cylinder_10_0', 'Cylinder_20_0', 'Cylinder_30_0',
@@ -336,7 +333,7 @@ def test2():
 
         # set up sampled structure
         struc_teste = Structure(structure, end_cap=True)
-        dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True, delta_cm=up)
+        dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True)
         dvh_data = prepare_dvh_data(dhist, chist)
         # dvh_data['Dmin'] = dmin
         # Setup DVH metrics class and get DVH DATA
@@ -381,10 +378,11 @@ def test2():
     test_table = pd.DataFrame(res).T
     print(test_table)
 
-    mask = np.logical_or(delta > lim, delta < -lim)
-    result.index = mask.index
-    print(result.loc[mask['D0.03cc']])
-    print(result.loc[mask['D95']])
+    return test_table
+    # mask = np.logical_or(delta > lim, delta < -lim)
+    # result.index = mask.index
+    # print(result.loc[mask['D0.03cc']])
+    # print(result.loc[mask['D95']])
 
 
 def test3(plot_curves=True):
@@ -393,7 +391,6 @@ def test3(plot_curves=True):
     struc_dir = '/home/victor/Dropbox/Plan_Competition_Project/testdata/DVH-Analysis-Data-Etc/STRUCTURES'
     dose_grid_dir = '/home/victor/Dropbox/Plan_Competition_Project/testdata/DVH-Analysis-Data-Etc/DOSE GRIDS'
     st = 2
-    up = (0.5, 0.5, 0.5)
 
     snames = ['Sphere_10_0', 'Sphere_30_0',
               'Cylinder_10_0', 'Cylinder_30_0',
@@ -442,7 +439,7 @@ def test3(plot_curves=True):
             # get dose
             dose_file = test_data[k]
             dicom_dose = ScoringDicomParser(filename=dose_file)
-            dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True, delta_cm=up)
+            dhist, chist = struc_teste.calculate_dvh(dicom_dose, upsample=True)
             dvh_data = prepare_dvh_data(dhist, chist)
             str_result[k] = dvh_data
 
@@ -484,8 +481,8 @@ def test3(plot_curves=True):
                 adata = an_data[s_key][grad]
                 calc_data = result[s_key][grad]
                 plt.figure()
-                plt.plot(adata['dose_axis'], adata['data'], '.', label='Analytical DVH')
-                plt.plot(calc_data['dose_axis'], calc_data['data'], '.', label='Software DVH')
+                plt.plot(adata['dose_axis'], adata['data'], label='Analytical DVH')
+                plt.plot(calc_data['dose_axis'], calc_data['data'], label='Software DVH')
                 plt.legend(loc='best')
                 plt.xlabel('Dose (cGy)')
                 plt.ylabel('Volume (cc)')
@@ -495,10 +492,7 @@ def test3(plot_curves=True):
 
 
 if __name__ == '__main__':
-    # rs_file = r'/home/victor/Dropbox/Plan_Competition_Project/FantomaPQRT/RS.PQRT END TO END.dcm'
-    # rd_file = r'/home/victor/Dropbox/Plan_Competition_Project/FantomaPQRT/RD.PQRT END TO END.Dose_PLAN.dcm'
-    up = (0.83, 0.83, 1)
-    # up = (2.5, 2.5, 1)
+
     # rd_file = r'/home/victor/Dropbox/Plan_Competition_Project/Eclipse Plans/Venessa IMRT Eclipse/RD-Eclipse-Venessa-IMRTDose.dcm'
     # rs_file = r'/home/victor/Dropbox/Plan_Competition_Project/Competition Package/DICOM Sets/RS.1.2.246.352.71.4.584747638204.208628.20160204185543.dcm'
 
@@ -508,7 +502,7 @@ if __name__ == '__main__':
     # dvh_file = r'/media/victor/TOURO Mobile/COMPETITION 2017/Send to Victor - Jan10 2017/Norm Res with CT Images/RD.1.2.246.352.71.7.584747638204.1746016.20170110164605.dvh'
 
     f = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/PlanIQ Criteria TPS PlanIQ matched str names - TXT Fromat - Last mod Jan23.txt'
-    constrains_all, scores_all = read_scoring_criteria(f)
+    constrains_all, scores_all, criteria = read_scoring_criteria(f)
 
     dose = ScoringDicomParser(filename=rd_file)
     struc = ScoringDicomParser(filename=rs_file)
@@ -526,40 +520,34 @@ if __name__ == '__main__':
                 if structure['name'] in list(scores_all.keys()):
                     ecl_dvh = ecl_DVH[structure['id']]['data']
                     ecl_dmax = ecl_DVH[structure['id']]['max'] * 100  # to cGy
-                    # if ecl_dvh[0] < 50.0:
-                    # struc = deepcopy(structure)
                     struc_teste = Structure(structure, end_cap=end_cap)
                     # struc['planes'] = struc_teste.planes
                     # dicompyler_dvh = get_dvh(structure, dose)
-
-                    dhist, chist = struc_teste.calculate_dvh(dose, upsample=True, delta_cm=up)
+                    fig, ax = plt.subplots()
+                    fig.set_figheight(12)
+                    fig.set_figwidth(20)
+                    dhist, chist = struc_teste.calculate_dvh(dose, upsample=True)
                     max_dose = get_dvh_max(chist)
-                    plt.figure()
-                    plt.plot(dhist, chist, label='Up sampled - Dmax: %1.1f cGy' % max_dose)
-                    plt.hold(True)
-                    plt.plot(ecl_dvh, label='Eclipse - Dmax: %1.1f cGy' % ecl_dmax)
+
+                    ax.plot(dhist, chist, label='Up sampled - Dmax: %1.1f cGy' % max_dose)
+                    fig.hold(True)
+                    ax.plot(ecl_dvh, label='Eclipse - Dmax: %1.1f cGy' % ecl_dmax)
                     dvh_data = prepare_dvh_data(dhist, chist)
 
                     txt = structure['name'] + ' volume (cc): %1.1f - end_cap: %s ' % (
                         ecl_dvh[0], str(end_cap))
-                    plt.title(txt)
+                    ax.set_title(txt)
                     # nup = get_dvh_max(dicompyler_dvh['data'])
                     # plt.plot(dicompyler_dvh['data'], label='Software DVH - Dmax: %1.1f cGy' % nup)
-                    plt.legend(loc='best')
-                    plt.xlabel('Dose (cGy)')
-                    plt.ylabel('Volume (cc)')
-
+                    ax.legend(loc='best')
+                    ax.set_xlabel('Dose (cGy)')
+                    ax.set_ylabel('Volume (cc)')
+                    fname = txt + '.png'
+                    fig.savefig(fname, format='png', dpi=100)
                     dvhs[structure['name']] = dvh_data
-
-        plt.show()
 
     end = time.time()
 
     print('Total elapsed Time (min):  ', (end - st) / 60)
 
     # print(dvhs)
-
-
-
-    # for structure in structures.values():
-    #     print(structure['id'], structure['name'])

dosimetric.py
@@ -183,13 +183,16 @@ def read_scoring_criteria(f):
 
     # get objectives
     objectives = []
+    obl = []
+    obh = []
     for row in df[['Objective(s)']].iterrows():
         cval = np.asarray(re.findall("\d+\.?\d+", row[1].to_string()), dtype=float)
         cval.sort()
         if cval[0] > 1:
             # All doses to cGy
             cval *= cGy
-
+        obl.append(cval[0])
+        obh.append(cval[1])
         objectives.append(cval)
 
     scores_arr = []
@@ -198,6 +201,8 @@ def read_scoring_criteria(f):
         scores_arr.append(score_i)
 
     df['value_score'] = objectives
+    df['value_low'] = obl
+    df['value_high'] = obh
     df['scores_array'] = scores_arr
 
     dfi = df.set_index('structures_name')
@@ -222,7 +227,9 @@ def read_scoring_criteria(f):
         scores_all[name] = scores_tmp
         constrains_all[name] = constrains_tmp
 
-    return constrains_all, scores_all
+    criteria = ['constrain', 'constrain_value', 'constrains_type', 'value_low', 'value_high', 'Max Score']
+
+    return constrains_all, scores_all, dfi[criteria]
 
 
 class Competition2017(object):
@@ -235,6 +242,7 @@ class Competition2017(object):
 
 if __name__ == '__main__':
     f = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/PlanIQ Criteria TPS PlanIQ matched str names - TXT Fromat - Last mod Jan23.txt'
+    cGy = 100.0
     df = pd.read_csv(f, sep='\t')
     df = df[['Plan Quality Metric Component', 'Objective(s)', 'Max Score']].dropna()
     df['Max Score'] = df['Max Score'].apply(lambda x: x.split('*')[0]).astype(float)
@@ -273,10 +281,12 @@ if __name__ == '__main__':
                 if 'Homogeneity' in crit[1]:
                     constrains_keys.append('HI')
                     cv = float(re.findall("\d+\.\d+", crit[2])[0])
+                    cv *= cGy
                     constrains_values.append(cv)
                 elif 'Conformation' in crit[1]:
                     constrains_keys.append('CI')
                     cv = float(re.findall("\d+\.\d+", crit[2])[0])
+                    cv *= cGy
                     constrains_values.append(cv)
             elif 'cc' in crit[2]:
                 constrains_keys.append('Dcc')
@@ -296,15 +306,17 @@ if __name__ == '__main__':
     df['constrains_type'] = constrains_types
 
     # get objectives
-    cGy = 100.0
     objectives = []
+    obl = []
+    obh = []
     for row in df[['Objective(s)']].iterrows():
         cval = np.asarray(re.findall("\d+\.?\d+", row[1].to_string()), dtype=float)
         cval.sort()
         if cval[0] > 1:
             # All doses to cGy
             cval *= cGy
-
+        obl.append(cval[0])
+        obh.append(cval[1])
         objectives.append(cval)
 
     scores_arr = []
@@ -313,6 +325,8 @@ if __name__ == '__main__':
         scores_arr.append(score_i)
 
     df['value_score'] = objectives
+    df['value_low'] = obl
+    df['value_high'] = obh
     df['scores_array'] = scores_arr
 
     dfi = df.set_index('structures_name')
@@ -336,3 +350,7 @@ if __name__ == '__main__':
 
         scores_all[name] = scores_tmp
         constrains_all[name] = constrains_tmp
+
+    criteria = ['constrain', 'constrain_value', 'constrains_type', 'value_low', 'value_high', 'Max Score']
+
+    # dfi[criteria].to_excel('criteria_2017.xlsx')

dvhcalc.py
@@ -19,7 +19,7 @@ from joblib import delayed
 from matplotlib.path import Path
 from numba import njit
 
-from dev.geometry import calc_area
+from dev.geometry import calculate_contour_areas_numba
 from dicomparser import ScoringDicomParser
 from dvhdoses import get_dvh_min, get_dvh_max, get_dvh_mean
 
@@ -334,34 +334,6 @@ def calculate_contour_areas(plane):
         cArea = abs(cArea / 2.0)
         # Remove the z coordinate from the xyz point tuple
         data = list(map(lambda x: x[0:2], contour['contourData']))
-
-        # Add the contour area and points to the list of contours
-        contours.append({'area': cArea, 'data': data})
-
-        # Determine which contour is the largest
-        if cArea > largest:
-            largest = cArea
-            largestIndex = c
-
-    return contours, largestIndex
-
-
-def calculate_contour_areas_numba(plane):
-    """Calculate the area of each contour for the given plane.
-       Additionally calculate and return the largest contour index."""
-
-    # Calculate the area for each contour in the current plane
-    contours = []
-    largest = 0
-    largestIndex = 0
-    for c, contour in enumerate(plane):
-        # Create arrays for the x,y coordinate pair for the triangulation
-        x = contour['contourData'][:, 0]
-        y = contour['contourData'][:, 1]
-
-        cArea = calc_area(x, y)
-        # Remove the z coordinate from the xyz point tuple
-        data = np.asarray(list(map(lambda x: x[0:2], contour['contourData'])))
 
         # Add the contour area and points to the list of contours
         contours.append({'area': cArea, 'data': data})

scoring.py
@@ -3,7 +3,7 @@ from joblib import Parallel
 from joblib import delayed
 import matplotlib.pyplot as plt
 from conformality import CalculateCI
-from dev.dvhcalculation import calc_dvhs_upsampled
+from dev.dvhcalculation import calc_dvhs_upsampled, Structure
 from dosimetric import read_scoring_criteria
 from dvhcalc import get_dvh, load, calc_dvhs
 from dvhdata import DVH, CalculateVolume
@@ -16,6 +16,9 @@ from collections import OrderedDict
 import scipy.interpolate as itp
 
 logger = logging.getLogger('scoring')
+
+
+# pd.options.display.float_format = '{:.2f}'.format
 
 
 def get_dvh_files(root_path):
@@ -122,7 +125,6 @@ class DVHMetrics(object):
         self.fd_cc = itp.interp1d(self.volume_cc, self.dose_axis, fill_value='extrapolate')  # cc
 
     def eval_constrain(self, key, value):
-
         # TODO refactor using dictionary
         if key == 'Global Max':
             return self.data['max']
@@ -153,26 +155,26 @@ class DVHMetrics(object):
         """ Return the maximum dose (in cGy) that a specific volume (in percent)
             receives. i.e. D90, D20."""
 
-        return self.fd(volume)
+        return float(self.fd(volume))
 
     def GetDoseConstraintCC(self, volumecc):
         """ Return the maximum dose (in cGy) that a specific volume in cc."""
 
-        return self.fd_cc(volumecc)
+        return float(self.fd_cc(volumecc))
 
     def GetVolumeConstraint(self, dose):
 
         """ Return the volume (in percent) of the structure that receives at
             least a specific dose in cGy. i.e. V100, V150. fix by Victor Gabriel"""
 
-        return self.fv(dose)
+        return float(self.fv(dose))
 
     def GetVolumeConstraintCC(self, dose):
 
         """ Return the volume (in percent) of the structure that receives at
             least a specific dose in cGy. i.e. V100, V150. fix by Victor Gabriel"""
 
-        return self.fv_cc(dose)
+        return float(self.fv_cc(dose))
 
     def get_volume(self):
         return self.data['data'][0]
@@ -201,8 +203,8 @@ class Scoring(object):
 
     def save_score_results(self, out_file):
         if self.dvhs:
-            score_results = pd.DataFrame(self.scoring_result)
-            constrains_results = pd.DataFrame(self.constrains_values)
+            score_results = pd.DataFrame(self.scoring_result).T
+            constrains_results = pd.DataFrame(self.constrains_values).T
             writer = pd.ExcelWriter(out_file)
             score_results.to_excel(writer, 'scores')
             constrains_results.to_excel(writer, 'constrains')
@@ -241,7 +243,7 @@ class Scoring(object):
 
         for key, values in self.constrains.items():
             if key == 'GLOBAL MAX':
-                max_dose = max([self.dvhs[k1]['max'] for k1 in self.dvhs.keys()])
+                max_dose = self.rtdose.global_max
                 self.dvhs['GLOBAL MAX'] = {'data': np.asarray([1, 100]),
                                            'max': max_dose,
                                            'mean': 0,
@@ -252,6 +254,7 @@ class Scoring(object):
             else:
                 dvh_values = self.dvhs[key]
 
+            # dvh_values = self.dvhs[key]
             # print(dvh_values)
 
             dvh_metrics = DVHMetrics(dvh_values)
@@ -268,7 +271,7 @@ class Scoring(object):
                         nk = self.dvhs[key]['key']
                         values_constrains[k] = self.check_dmax_inside(self.structures[nk]['name'], self.dvhs)
                     if key == 'GLOBAL MAX':
-                        values_constrains[k] = self.dvhs['GLOBAL MAX']['max']
+                        values_constrains[k] = self.rtdose.global_max
                 except:
                     nk = self.dvhs[key]['key']
                     sname = self.structures[nk]['name']
@@ -289,9 +292,8 @@ class Scoring(object):
         return self.score_result
 
     def get_conformity(self, nk, value):
-        PITV, CV = CalculateCI(self.rtdose, self.structures[nk], value)
-        TV = CalculateVolume(self.structures[nk])
-        cindex = CV * CV / (TV * PITV)
+        stmp = Structure(self.structures[nk])
+        cindex = stmp.calc_conformation_index(self.rtdose, value)
         return cindex
 
     @staticmethod
@@ -373,7 +375,7 @@ class EvalCompetition(object):
 
 
 class Participant(object):
-    def __init__(self, rp_file, rs_file, rd_file, dvh_file='', upsample=''):
+    def __init__(self, rp_file, rs_file, rd_file, dvh_file='', upsample='', end_cap=False):
         """
             Class to encapsulate all plan participant planning data to eval using pyplanscoring app
         :param rp_file: path to DICOM-RTPLAN file
@@ -393,8 +395,10 @@ class Participant(object):
         self.participant_name = ''
         self.structure_names = []
         self.up_sample = upsample
+        self.end_cap = end_cap
 
     def set_participant_data(self, participant_name):
+        # TODO IMPLEMENT CALCULATING ONLY SCORED STRUCTURES DVH
         self.participant_name = participant_name
         self.plan_data = self.rp_dcm.GetPlan()
         self.tps_info = self.rd_dcm.get_tps_data()
@@ -405,9 +409,12 @@ class Participant(object):
         if not self.dvh_file:
             p = os.path.splitext(self.rd_file)
             self.dvh_file = p[0] + self.up_sample + '.dvh'
+            if self.end_cap:
+                self.dvh_file = p[0] + self.up_sample + 'end_cap.dvh'
             if not os.path.exists(self.dvh_file):
                 if self.up_sample:
-                    cdvh = calc_dvhs_upsampled(participant_name, self.rs_file, self.rd_file, out_file=self.dvh_file)
+                    cdvh = calc_dvhs_upsampled(participant_name, self.rs_file, self.rd_file, out_file=self.dvh_file,
+                                               end_cap=self.end_cap)
                     self._save_dvh_fig(cdvh, self.rd_file)
                 else:
                     cdvh = calc_dvhs(participant_name, self.rs_file, self.rd_file, out_file=self.dvh_file)
@@ -418,6 +425,9 @@ class Participant(object):
         _, filename = os.path.split(dest)
 
         fig_name = p[0] + '_RD_calc_' + self.up_sample + 'DVH.png'
+
+        if self.end_cap:
+            fig_name = p[0] + '_RD_calc_' + self.up_sample + '_END_CAPPED_DVH.png'
 
         fig, ax = plt.subplots()
         fig.set_figheight(12)
@@ -443,29 +453,53 @@ class Participant(object):
 
 
 if __name__ == '__main__':
-    # root_path = r'C:\Users\Victor\Dropbox\Plan_Competition_Project\Eclipse Plans\Saad RapidArc Eclipse\Saad RapidArc Eclipse'
-    # root_path = r'C:\Users\Victor\Dropbox\Plan_Competition_Project\Eclipse Plans\Venessa IMRT Eclipse'
-    # dt, val = get_participant_folder_data('test', root_path)
-    f_2017 = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/PlanIQ Criteria TPS PlanIQ matched str names - TXT Fromat - Last mod Jan23.txt'
-    constrains, scores = read_scoring_criteria(f_2017)
-
-    # rs = r'D:\Dropbox\Plan_Competition_Project\testdata\RTSS.dcm'
-    # rd = r'D:\Dropbox\Plan_Competition_Project\testdata\RTDOSE.dcm'
-    # rp = r'D:\Dropbox\Plan_Competition_Project\testdata\RTPLAN.dcm'
 
     rs_file = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/RS.1.2.246.352.71.4.584747638204.248648.20170123083029.dcm'
     rd_file = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/RD.1.2.246.352.71.7.584747638204.1750110.20170123082607.dcm'
     rp = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/RP.1.2.246.352.71.5.584747638204.952069.20170122155706.dcm'
 
-    #
-    obj = Participant(rp, rs_file, rd_file)
-    obj.set_participant_data('Ahmad')
-    val = obj.eval_score(constrains_dict=constrains, scores_dict=scores)
+    f_2017 = r'/home/victor/Dropbox/Plan_Competition_Project/competition_2017/All Required Files - 23 Jan2017/PlanIQ Criteria TPS PlanIQ matched str names - TXT Fromat - Last mod Jan23.txt'
 
-    obj = Participant(rp, rs_file, rd_file, upsample='_up_sampled_')
+    constrains, scores, criteria = read_scoring_criteria(f_2017)
+
+    obj = Participant(rp, rs_file, rd_file, upsample='_up_sampled_', end_cap=True)
     obj.set_participant_data('Ahmad')
     val1 = obj.eval_score(constrains_dict=constrains, scores_dict=scores)
+    #
+    # print('Radiation Knowledge Plan Competition - 2017 ')
+    # print('Ahmad Score (no up-sampling):', val)
+    print('Ahmad Score end capped:', val1)
 
-    print('Radiation Knowledge Plan Competition - 2017 ')
-    print('Ahmad Score (no up-sampling):', val)
-    print('Ahmad Score:', val1)
+    # TODO ENCAPSULATE ON SAVE REPORT FUNCTION
+
+    crt_values = obj.score_obj.constrains_values.copy()
+    score_results = pd.DataFrame(obj.score_obj.scoring_result).T
+    constrains_results = pd.DataFrame(obj.score_obj.constrains_values).T
+
+    criteria.index = [name.upper() for name in criteria.index]
+
+    s_names = criteria.index.unique()
+    constrains_all = {}
+    scores_all = {}
+    new_idx = []
+    new_results = []
+    scores_tmp = []
+    ctr = []
+    report = []
+    for name in s_names:
+        # if name == 'PTV56':
+        sc_tmp = criteria.loc[[name]]
+        ctr_tmp = constrains_results.loc[[name]].dropna(axis=1)
+        score_tmp = score_results.loc[[name]].dropna(axis=1)
+
+        for res in ctr_tmp.columns:
+            mask = sc_tmp['constrain'] == res
+            tmp = sc_tmp.loc[mask].copy()
+            tmp['Result'] = ctr_tmp[res].values[0]
+            tmp['Raw Score'] = score_tmp[res].values[0]
+            report.append(tmp)
+
+    df_report = pd.concat(report)
+
+    out = '/home/victor/Dropbox/Plan_Competition_Project/Results_ahmad_small_end_capped.xlsx'
+    df_report.to_excel(out)