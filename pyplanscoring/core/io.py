37b9d4b2a7e2117a11ec36612f4fbeac2f8163fa
Victor Gabriel Leandro Alves <victorgabr@gmail.com>
2020-07-13 21:52:56 -0400

    Update io.py
    Adding a more robust Dicom folder parsing

--- pyplanscoring/core/io.py
+++ pyplanscoring/core/io.py
@@ -134,20 +134,9 @@ def get_participant_folder_data(root_path):
         Provide all participant required files (RP,RS an RD DICOM FILES)
     :param root_path: participant folder
     :return: Pandas DataFrame containing path to files
+
     """
-    files = [os.path.join(root, name) for root, dirs, files in os.walk(root_path) for name in files if
-             name.endswith(('.dcm', '.DCM'))]
-
-    filtered_files = {'rtdose': False, 'rtplan': False, 'rtss': False}
-    for f in files:
-        obj = PyDicomParser(filename=f)
-        rt_type = obj.GetSOPClassUID()
-        # fix halcyon SOP class UI
-        if rt_type is None:
-            rt_type = obj.ds.Modality.lower()
-
-        if rt_type in ['rtdose', 'rtplan', 'rtss']:
-            filtered_files[rt_type] = f
+    filtered_files = parse_patient_dicom_folder(root_path)
 
     missing_files = [key for key, value in filtered_files.items() if value is False]
 
@@ -155,3 +144,48 @@ def get_participant_folder_data(root_path):
         return filtered_files, True
     else:
         return missing_files, False
+
+
+def GetSOPClassUID(ds):
+    """Determine the SOP Class UID of the current file."""
+
+    if "SOPClassUID" not in ds:
+        return None
+    if ds.SOPClassUID == "1.2.840.10008.5.1.4.1.1.481.2":
+        return "rtdose"
+    elif ds.SOPClassUID == "1.2.840.10008.5.1.4.1.1.481.3":
+        return "rtss"
+    elif ds.SOPClassUID == "1.2.840.10008.5.1.4.1.1.481.5":
+        return "rtplan"
+    elif ds.SOPClassUID == "1.2.840.10008.5.1.4.1.1.2":
+        return "ct"
+    else:
+        return None
+
+
+def parse_patient_dicom_folder(pt_folder):
+    """
+        Method parse an exported patient folder
+    :param pt_folder: path to the patient folder
+    :return: dictionary with rtdose, rtss keys.
+    """
+    dicom_files = [os.path.join(pt_folder, f) for f in os.listdir(pt_folder)]
+    filtered_files = {"rtdose": [], "rtss": [], "rtplan": [], "ct": []}
+    for dicom_file in dicom_files:
+        if os.path.isfile(dicom_file):
+            try:
+                ds = pydicom.read_file(dicom_file, defer_size=100, force=True)
+                rt_type = GetSOPClassUID(ds)
+                if rt_type == "rtdose":
+                    filtered_files["rtdose"] = dicom_file
+                if rt_type == "rtss":
+                    filtered_files["rtss"] = dicom_file
+                if rt_type == "rtplan":
+                    filtered_files["rtplan"] = dicom_file
+                if rt_type == "ct":
+                    filtered_files["ct"].append(dicom_file)
+            except IOError:
+                print("{os.path.split(dicom_file)[-1]} is not dicom")
+
+    return filtered_files
+
